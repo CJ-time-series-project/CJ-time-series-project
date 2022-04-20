7b1047a1fa7fe9e07d7ec1ea03f56ccf2390e97d
Jerry Nolf <nolf.jerry@gmail.com>
2022-04-20 10:24:04 -0500

    updated

--- acquire.py
+++ acquire.py
@@ -24,7 +24,7 @@ def get_superstore_data(use_cache=True):
     df.to_csv(filename)
     return df
 
-    def prep_superstore_data(df):
+def prep_superstore_data(df):
     df.drop(columns=['Unnamed: 0', 'Region ID', 'Product ID', 'Customer ID', 'Category ID', 'Country'], inplace=True)
     df['Order Date'] = pd.to_datetime(df['Order Date'])
     df.set_index('Order Date', inplace=True)
